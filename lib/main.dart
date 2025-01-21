3ffa30c3fd3f9176127ecbd5f3f40088f08ef07f
doublealpha21 <praise8fakorede@gmail.com>
2025-01-22 00:13:40 +0100

    Wallet Provider Creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:crypto_wallet/wallet_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:crypto_wallet/wallet_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider<WalletProvider>(
+    create: (context) => WalletProvider(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +16,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    final walletProvider = Provider.of<WalletProvider>(context);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Crypto Wallet',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Crpto Wallet'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () async
+                {
+                 final mnemonic = walletProvider.generateMnemonic();
+                 final privateKey = await walletProvider.getPrivateKey(mnemonic);
+                 final publicKey = await walletProvider.getPublicKey(privateKey);
+                },
+                child: Text('Generate Wallet'),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyApp(),
     );
   }
 }
